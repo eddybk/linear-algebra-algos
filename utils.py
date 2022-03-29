4694d24cf0cce8210ef276654bf1aff929831787
unknown <krasedgar23@gmail.com>
2022-03-29 21:04:24 +0200

    Fixed formatting and naming issues in utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,13 @@
 import numpy as np
 def plot_vec(v: np.ndarray, *, scale=(0, 100)):
+    import matplotlib.pyplot as plt
+
     x = v[0]
     y = v[1]
 
-    import matplotlib.pyplot as plt
-
-
-
-    # plot
-    fig, ax = plt.subplots()
-
+    _, ax = plt.subplots()
     ax.scatter(x, y)
-
+    
     ax.set(xlim=(0, scale[0] + 1), xticks=np.arange(0, scale[0] + 1),
        ylim=(0, scale[1] + 1), yticks=np.arange(1, scale[1] + 1))
 
