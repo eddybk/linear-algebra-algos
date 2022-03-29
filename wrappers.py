129c8893bcc1f3b693681926145f28c85c0fb6c6
unknown <krasedgar23@gmail.com>
2022-03-29 20:20:42 +0200

    Seperated wrappers into wrappers.py

---
+++ wrappers.py
@@ -0,0 +1,22 @@
+import numpy as np
+import random
+
+def matrix(rows: int, cols: int, *, random_val:bool=False, init_value:float=0) -> np.array:
+    if random_val:
+        return np.array([[random.uniform(-1, 1) for _ in range(cols)] for _ in range(rows)])
+    return np.array([[init_value for _ in range(cols)] for _ in range(rows)])
+
+def vec2(x: float, y: float, /):
+    return np.array([x, y])
+
+def vec3(x, y, z, /) -> np.ndarray:
+    return np.array([x, y, z])
+
+def vec(*args: float):
+    for value in args:
+        if not isinstance(value, float):
+            if not isinstance(value, int):
+                print(f"Could not instanciate a vector with value {value=} of type {type(value)}. Returned None instead")
+                return None
+    return np.array(args)
+    
