7f1b631f416a1a9a91090310641a76552ece91ef
unknown <krasedgar23@gmail.com>
2022-04-01 23:03:53 +0200

    Added add_vecs and hats functions

--- utils.py
+++ utils.py
@@ -1,4 +1,12 @@
 import numpy as np
+from typing import Optional
+
+def add_vecs(vecs: np.ndarray, /):
+    ret = np.array([0 for _ in range(len(vecs))])
+    for i in range(len(vecs)):
+        ret += vecs[i]
+    return ret
+
 def plot_vec(v: np.ndarray, *, scale=(0, 100)):
     import matplotlib.pyplot as plt
 
@@ -12,3 +20,20 @@ def plot_vec(v: np.ndarray, *, scale=(0, 100)):
        ylim=(0, scale[1] + 1), yticks=np.arange(scale[0], scale[1] + 1))
 
     plt.show()
+
+def hats(*, dimensions: int, root: list=[0.0], scalars: list=[0.0]) -> Optional[np.ndarray]:
+    scalars = np.array(scalars)
+    root = np.array(root)
+    dimr = range(dimensions)
+    if len(scalars) == 1 and scalars[0] == 0:
+        ret = np.array([np.array([0 for _ in dimr]) for _ in dimr])
+        for i in dimr:
+            ret[i][i] = 1
+        return ret
+    elif len(scalars) == dimensions:
+        ret = hats(dimensions=dimensions)
+        for i in dimr:
+            ret[i] = ret[i] * scalars[i]
+        return ret
+    else:
+        return None
