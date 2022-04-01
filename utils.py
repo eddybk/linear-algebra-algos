7c6361f0aa82d4dfc67eac8ad4d7bd181230482d
unknown <krasedgar23@gmail.com>
2022-04-01 18:20:26 +0200

    Fixed bug with plot_vec

--- utils.py
+++ utils.py
@@ -7,8 +7,8 @@ def plot_vec(v: np.ndarray, *, scale=(0, 100)):
 
     _, ax = plt.subplots()
     ax.scatter(x, y)
-    
-    ax.set(xlim=(0, scale[0] + 1), xticks=np.arange(0, scale[0] + 1),
-       ylim=(0, scale[1] + 1), yticks=np.arange(1, scale[1] + 1))
+
+    ax.set(xlim=(0, scale[0] + 1), xticks=np.arange(scale[0], scale[1] + 1),
+       ylim=(0, scale[1] + 1), yticks=np.arange(scale[0], scale[1] + 1))
 
     plt.show()
